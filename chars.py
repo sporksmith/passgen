3f3732956166bb2f6b4e721cfeb1592ec7f0e38a
Jim Newsome <jnewsome@andrew.cmu.edu>
2010-12-06 17:20:31 -0500

    start random-character based (copied from pairs.py)

---
+++ chars.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import random
+import string
+import sys
+import math
+
+def pairs(numpairs, r=random.SystemRandom(), useCaps=True):
+    vowels = set('aeiou')
+    cons = set(string.lowercase) - vowels
+
+    # r.choice requires an indexable data structure
+    vowels = list(vowels)
+    cons = list(cons)
+
+    rand_pair = lambda: (r.choice(cons) + r.choice(vowels))
+
+    pw = [rand_pair() for i in xrange(numpairs)]
+    if useCaps:
+        pw = map(lambda x: r.choice([x, x.upper()]), pw)
+    pw = ''.join(pw)
+    entropy = math.log((len(cons)*len(vowels)*(useCaps and 4 or 1))**numpairs)/math.log(2)
+    return (pw, entropy)
+
+if __name__ == '__main__':
+    (pw, entropy) = pairs(int(sys.argv[1]), useCaps=(sys.argv[2]=='t'))
+    print pw
+    print 'entropy: %f bits' % entropy
+
