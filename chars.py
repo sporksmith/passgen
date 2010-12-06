4c5b05ed5b228093f47ff3a9cc05df0c3e40ddf9
Jim Newsome <jnewsome@andrew.cmu.edu>
2010-12-06 18:02:01 -0500

    refactored 'chars' password generator into a class. plan is to refactor the others similarly, and have them inherit from a common superclass. this will make it easier to fiddle with comparing entropy vs usability of different options

--- chars.py
+++ chars.py
@@ -5,23 +5,37 @@ import string
 import sys
 import math
 
-def chars(numchars, r=random.SystemRandom()):
-    candidates = set()
-    candidates.update(string.ascii_lowercase)
-    candidates.update(string.ascii_uppercase)
-    candidates.update(string.punctuation)
-    candidates.update(string.digits)
+class charsPass:
+    def __init__(self, r=random.SystemRandom(), useLower=True, useUpper=True, usePunct=True, useDigits=True):
+        self.r = r
 
-    # r.choice requires an indexable data structure
-    candidates = list(candidates)
+        self.candidates = set()
+        if useLower: self.candidates.update(string.ascii_lowercase)
+        if useUpper: self.candidates.update(string.ascii_uppercase)
+        if usePunct: self.candidates.update(string.punctuation)
+        if useDigits: self.candidates.update(string.digits)
+        self.candidates = list(self.candidates)
 
-    pw = [r.choice(candidates) for i in xrange(numchars)]
-    pw = ''.join(pw)
-    entropy = math.log(len(candidates)**numchars)/math.log(2)
-    return (pw, entropy)
+    def entropy_of_length(self, length):
+        return math.log(len(self.candidates)**length)/math.log(2)
+
+    def length_of_entropy(self, entropy):
+        length = 1
+        while (entropy_of_length(length) < entropy):
+            length += 1
+        return length
+
+    def generate_length(self, length):
+        pw = [self.r.choice(self.candidates) for i in xrange(length)]
+        pw = ''.join(pw)
+        return (pw, self.entropy_of_length(length))
+
+    def generate_entropy(self, entropy):
+        return self.generate_length(self.length_of_entropy(entropy))
+
 
 if __name__ == '__main__':
-    (pw, entropy) = chars(int(sys.argv[1]))
+    g = charsPass(useLower=True, useUpper=True, usePunct=True, useDigits=True)
+    (pw, entropy) = g.generate_length(int(sys.argv[1]))
     print pw
     print 'entropy: %f bits' % entropy
-
