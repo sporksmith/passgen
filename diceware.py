c86cae8c7dd4eaad018527f8c608b5084f42de77
Jim Newsome <spork@sporksmith.net>
2022-12-24 09:07:19 -0600

    update diceware.py for python3

--- diceware.py
+++ diceware.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import random
 import string
@@ -9,9 +9,9 @@ import math
 def diceware(numwords, r=random.SystemRandom(), wordsfile='diceware.wordlist.asc.txt'):
     f = open(wordsfile)
     lines = f.readlines()
-    words = map(lambda x: x.split()[1], lines)
+    words = [w for w in map(lambda x: x.split()[1], lines)]
 
-    rv = [r.choice(words).capitalize() for i in xrange(numwords)]
+    rv = [r.choice(words).capitalize() for i in range(numwords)]
 
     pw = ''.join(rv)
     entropy = math.log(len(words)**numwords)/math.log(2)
@@ -19,5 +19,5 @@ def diceware(numwords, r=random.SystemRandom(), wordsfile='diceware.wordlist.asc
 
 if __name__ == '__main__':
     (pw, entropy) = diceware(int(sys.argv[1]))
-    print pw
-    print 'entropy: %f bits' % entropy
+    print(pw)
+    print('entropy: %f bits' % entropy)
